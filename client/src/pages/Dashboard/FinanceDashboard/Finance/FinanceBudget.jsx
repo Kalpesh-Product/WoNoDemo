798c030d66b7746a69589d4f88f48c5e14752f81
allanwono <allan.wono@gmail.com>
2025-06-13 12:05:12 +0530

    added checks for null & undefined

--- client/src/pages/Dashboard/FinanceDashboard/Finance/FinanceBudget.jsx
+++ client/src/pages/Dashboard/FinanceDashboard/Finance/FinanceBudget.jsx
@@ -91,13 +91,23 @@ const FinanceBudget = () => {
     },
   });
 
+  // const uniqueBuildings = Array.from(
+  //   new Map(
+  //     units.length > 0
+  //       ? units.map((loc) => [
+  //           loc.building._id, // use building._id as unique key
+  //           loc.building.buildingName,
+  //         ])
+  //       : []
+  //   ).entries()
+  // );
+
   const uniqueBuildings = Array.from(
     new Map(
       units.length > 0
-        ? units.map((loc) => [
-            loc.building._id, // use building._id as unique key
-            loc.building.buildingName,
-          ])
+        ? units
+            .filter((loc) => loc.building && loc.building._id)
+            .map((loc) => [loc.building._id, loc.building.buildingName])
         : []
     ).entries()
   );
@@ -105,7 +115,6 @@ const FinanceBudget = () => {
   const { mutate: requestBudget, isPending: requestBudgetPending } =
     useMutation({
       mutationFn: async (data) => {
-
         const response = await axios.post(
           `/api/budget/request-budget/${department._id}`,
           {
